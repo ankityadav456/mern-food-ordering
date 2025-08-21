ef398f503bd4b7caae4936a64f92777a19cbe679
Ankit G. Yadav <75722735+ankityadav456@users.noreply.github.com>
2025-08-21 11:42:11 +0530

    Update foodRoutes.js

--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -9,8 +9,8 @@ import {
 const router = express.Router();
 
 router.get("/", getAllFoodItems); // ✅ Get all food items
-// router.post("/", addFoodItem); // ✅ Add food item (Admin Only)
-// router.put("/:id", updateFoodItem); // ✅ Update food item (Admin Only)  <-- New Update Route
-// router.delete("/:id", deleteFoodItem); // ✅ Delete food item (Admin Only)
+router.post("/", addFoodItem); // ✅ Add food item (Admin Only)
+router.put("/:id", updateFoodItem); // ✅ Update food item (Admin Only)  <-- New Update Route
+router.delete("/:id", deleteFoodItem); // ✅ Delete food item (Admin Only)
 
 export default router;
